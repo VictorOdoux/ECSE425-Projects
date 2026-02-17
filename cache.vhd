e1eee23728511d357e14758e939b8c67bd8ee55e
Sakura7383 <cherli3c33@gmail.com>
2026-02-17 18:41:49 -0500

    add overall cache state structure

--- cache.vhd
+++ cache.vhd
@@ -30,7 +30,11 @@ end cache;
 architecture arch of cache is
 
 -- declare signals here
+	-- 32 bit addresses, cache data 128 bit blocks 
 	type t_cache_data is array(31 downto 0) of std_logic_vector(127 downto 0); 
+	
+	-- 32 = 2^5, 5 index bits, 4 offset bits (2 of which ignored), so 15 lower bits - 5 - 4 = 6 tag bits
+	-- cache info: 1 valid bit, 1 dirty bit, 6 tag bits
 	type t_cache_info is array(31 downto 0) of std_logic_vector(7 downto 0); 
 	signal data : t_cache_data; 
 	signal info : t_cache_info; 
@@ -42,4 +46,47 @@ begin
 
 -- make circuits here
 
+	cache_process : process(clock) 
+		variable count : integer range 0 to 15; -- 16B when interacting with mem
+		-- probably add to this
+	
+	begin
+		if (rising_edge(clock)) then
+			if (reset = '1') then
+				state <= main; 
+				-- clear everything to 0
+				if reset = '1' then 
+					data <= (others => (others => '0'));
+					info <= (others => (others => '0'));
+				end if; 
+				
+				s_waitrequest <= '1'; 
+				
+				m_addr <= 0; -- integer
+				m_read <= '0'; 
+				m_write <= '0'; 
+				
+				s_readdata <= (others => '0'); -- this format for std logic vectors
+				m_writedata <= (others => '0'); 
+			else -- reset != 1
+				case state is
+					when main => 
+						-- stuff happens, probably check cache miss vs hit here
+					
+					when memwrite => 
+						-- more stuff, send back to main when done
+						
+					when memread => 
+						-- also send back to main when done
+					
+					when transition => 
+						-- reset waitrequest, return to main
+						
+					when others => 
+						-- reset = 1 behavior? 
+						
+				end case;
+			end if; -- if reset
+		end if; -- if clock
+	end process; 
 end arch;
